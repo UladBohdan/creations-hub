fa5936e312707e4af54f77d66dc276d3336d32a3
UladBohdan <vlbohdan@gmail.com>
2016-03-19 23:25:21 +0300

    User page UI improved; stars added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,8 @@ class MainController < ApplicationController
 
   def index
     @creations = Creation.get_some_creations
+    @most_rated = Creation.all.order(rating: :desc).limit(5)
+    @most_recent = Creation.all.order(created_at: :desc).limit(5)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,19 +40,19 @@ ActiveRecord::Schema.define(version: 20160319184717) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "image"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
